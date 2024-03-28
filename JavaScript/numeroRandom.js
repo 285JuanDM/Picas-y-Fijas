b1b9a152c7f6e82f2ea81d968fa64be490e90bc3
285JuanDM <285juanda@gmail.com>
2024-03-27 20:34:40 -0500

    Se arregló como se genera el número secreto

--- JavaScript/numeroRandom.js
+++ JavaScript/numeroRandom.js
@@ -1,16 +1,13 @@
 const generarNumeroRandom = () => {
-    const digitos = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const digitos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     let numeroRandom = '';
 
-    const primerDigito = digitos[Math.floor(Math.random() * digitos.length)];
-    numeroRandom += primerDigito;
-
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 4; i++) {
         const indice = Math.floor(Math.random() * digitos.length);
         numeroRandom += digitos.splice(indice, 1)[0];
     }
 
-    return parseInt(numeroRandom);
+    return numeroRandom;
 }
 
 export { generarNumeroRandom };
